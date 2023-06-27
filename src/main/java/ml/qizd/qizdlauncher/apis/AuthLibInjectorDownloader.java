440df72069881486d3b9eebe067fc584f69e3116
Elisey Puzko <puzko.e02@gmail.com>
2023-06-27 20:58:12 +0300

    Changed authlib version to 1.2.3

--- src/main/java/ml/qizd/qizdlauncher/apis/AuthLibInjectorDownloader.java
+++ src/main/java/ml/qizd/qizdlauncher/apis/AuthLibInjectorDownloader.java
@@ -7,8 +7,8 @@ import java.net.URL;
 import java.nio.file.Path;
 
 public class AuthLibInjectorDownloader {
-    private static final String URL = "https://github.com/yushijinhun/authlib-injector/releases/download/v1.2.2/authlib-injector-1.2.2.jar";
-    public static final String INJECTOR_FILE = "authlib-injector-1.2.2.jar";
+    private static final String URL = "https://github.com/yushijinhun/authlib-injector/releases/download/v1.2.3/authlib-injector-1.2.3.jar";
+    public static final String INJECTOR_FILE = "authlib-injector-1.2.3.jar";
 
     public static void download(Downloader downloader) throws Exception {
         Downloader.Task task = downloader.taskFrom(new URL(URL), Path.of(Settings.getHomePath(), INJECTOR_FILE));
